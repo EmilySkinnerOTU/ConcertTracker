5038687c6dd6c89d4d11433d692274c4df85e38a
EmilySkinnerOTU <emily.skinner@ontariotechu.net>
2024-11-23 23:47:10 -0500

    Initial commit

---
+++ Server/routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/* Creates a route to each accessible page on the website*/
+router.get('/', function(req, res, next) {
+  res.render('mainPage', { title: 'Home Page' });
+});
+
+router.get('/aboutMe', function(req, res, next) {
+  res.render('aboutMe', { title: 'About Me' });
+});
+router.get('/mainPage', function(req, res, next) {
+  res.render('mainPage', { title: 'Home Page' });
+});
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+router.get('/contactMe', function(req, res, next) {
+  res.render('contactMe', { title: 'Contact Me' });
+});
+router.get('/submitComplete', function(req, res, next) {
+  res.render('submitComplete', { title: 'Submission Complete' });
+});
+
+module.exports = router;
